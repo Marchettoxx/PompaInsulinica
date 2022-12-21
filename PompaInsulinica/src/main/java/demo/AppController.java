ad6a508554bd93c2ad358a57d0a591919b19192c
AlessioTonelli <alessio.tonelli@studenti.unitn.it>
2022-12-21 16:19:57 +0100

    Ho inserito un ciaooo

--- PompaInsulinica/src/main/java/demo/AppController.java
+++ PompaInsulinica/src/main/java/demo/AppController.java
@@ -56,7 +56,7 @@ public class AppController {
         return "utente";
     }
 
-    /* Ho fatto il pull */
+    /* Ciaoooooo */
     /*@RequestMapping("/input")
     public String input(){
         return "input";
